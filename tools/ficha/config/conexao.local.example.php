09178d068a41b3386081287ac4e9c808c25546fb
danielaraujodasilva <danielaraujodasilva@gmail.com>
2026-04-27 21:13:45 -0300

    conexao BD meduri

--- tools/ficha/config/conexao.local.example.php
+++ tools/ficha/config/conexao.local.example.php
@@ -3,6 +3,6 @@ return [
     'host' => 'localhost',
     'port' => 3306,
     'database' => 'meduri',
-    'username' => 'seu_usuario_mysql',
-    'password' => 'sua_senha_mysql',
+    'username' => 'tatu_user',
+    'password' => 'Daniel*123',
 ];
